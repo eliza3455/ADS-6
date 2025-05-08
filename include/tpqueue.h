1001612fcb3104d21299017b01d354fc7d98bb00
eliza3455 <catra7902@gmail.com>
2025-05-08 16:04:13 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,9 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
 #include <stdexcept>
 
+template<typename T>
 class TPQueue {
  private:
   struct Node {
@@ -59,7 +58,8 @@ class TPQueue {
 };
 
 struct SYM {
-  char ch; // cppcheck-suppress unusedStructMember
+  char ch;  // cppcheck-suppress unusedStructMember
   int prior;
 };
+
 #endif  // INCLUDE_TPQUEUE_H_
